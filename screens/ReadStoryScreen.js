7732687dd3b18a941e3e79e59da3fc90f8c1f5c4
VAIBHAV JAIN <64785307+vibhu0209@users.noreply.github.com>
2021-09-19 16:02:00 +0530

    Update ReadStoryScreen.js

--- screens/ReadStoryScreen.js
+++ screens/ReadStoryScreen.js
@@ -47,11 +47,7 @@ export default class ReadStoryScreen extends React.Component {
     const newData = this.state.allStories.filter((item)=> {
       //applying filter for the inserted text in search bar
       const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
-      //const textData = text.toUpperCase();
-      // const textData = text.toUppercase();
-      //const textData = text.toupperCase();
-     // const textData = text.upperCase();
-
+      const textData = text.toUpperCase();
 
       return itemData.indexOf(textData) > -1;
     });
@@ -75,21 +71,15 @@ export default class ReadStoryScreen extends React.Component {
               <SearchBar
               placeholder="Type Here..."
              
-             // onChangeText={text => this.SearchFilterFunction(text)}
-             // onChangeText={() => this.SearchFilterFunction(text)}
-             // onChangeText={({text}) => this.SearchFilterFunction(text)}
-             // onChangeText={text => this.SearchFilterFunction(text)}
-
+               onChangeText={text => this.SearchFilterFunction(text)}
               value={this.state.search}
             />
           </View>
           
           <FlatList
                 data={this.state.search === "" ?  this.state.allStories: this.state.dataSource}
-                //renderItem={({ item }) => (
-                //renderItem={( item ) => (
-                //renderItem={({ item }) => {
-                // renderItem=>{({}){
+            
+                  renderItem={({ item }) => (
                   <View style={styles.itemContainer}>
                     <Text>  Title: {item.title}</Text>
                     <Text>  Author : {item.author}</Text>
